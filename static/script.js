f576390ced07d4573bc27c4908d319b77a013cd6
Hejty <pilat2006@gmail.com>
2024-11-08 00:03:05 +0100

    add: fetch specific task

--- static/script.js
+++ static/script.js
@@ -3,23 +3,29 @@ const API_LINK = 'http://localhost:3000/task'
 const displayCards = (data) => {
   const container = document.getElementById('tasks')
   data.forEach((item) => {
-    const card = document.createElement('div')
-    card.className = 'card'
-    card.id = item.id
-
-    const name = document.createElement('h3')
-    name.textContent = item.name
-    card.appendChild(name)
-
-    const description = document.createElement('p')
-    description.textContent = item.description
-    card.appendChild(description)
-
-    if (item.done) {
-      card.classList.add('done')
-    }
+    const card = displayTaskDetails(item)
     container.appendChild(card)
   })
+}
+
+const displayTaskDetails = (data) => {
+  const card = document.createElement('div')
+  card.className = 'card'
+  card.id = data.id
+
+  const name = document.createElement('h3')
+  name.textContent = data.name
+  card.appendChild(name)
+
+  const description = document.createElement('p')
+  description.textContent = data.description
+  card.appendChild(description)
+
+  if (data.done) {
+    card.classList.add('done')
+  }
+
+  return card
 }
 
 const fetchTasks = async () => {
@@ -27,11 +33,38 @@ const fetchTasks = async () => {
     const response = await fetch(API_LINK)
     const data = await response.json()
 
-    console.log(data)
-    displayCards(data)
+    if (data) displayCards(data)
   } catch (err) {
     console.error('Error fetching tasks:', err)
   }
 }
 
+const fetchSpecificTask = async (id) => {
+  try {
+    const response = await fetch(`${API_LINK}/${id}`)
+    const data = await response.json()
+    console.log(data)
+    if (data) {
+      const container = document.getElementById('specificTask')
+      const task = displayTaskDetails(data)
+      container.appendChild(task)
+    }
+  } catch (err) {
+    console.error('Error fetching task:', err)
+  }
+}
+
 document.addEventListener('DOMContentLoaded', fetchTasks)
+document
+  .getElementById('specificTaskForm')
+  .addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const taskId = document.getElementById('taskId').value
+    if (taskId) {
+      fetchSpecificTask(taskId)
+    } else {
+      document.getElementById('taskDetails').textContent =
+        'Please enter a valid task ID.'
+    }
+  })
